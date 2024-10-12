4c5485d82347a747b85b78710a5fa5cc6b8be806
zparilti <67382808+zparilti@users.noreply.github.com>
2024-10-11 23:32:53 -0400

    Add files via upload

---
+++ home.spec.js
@@ -0,0 +1,32 @@
+//https://www.automationexercise.com/test_cases
+// @ts-check
+
+// POM structure eases authoring and maintenance of test suites
+// PO represents parts of web application; home, products, etc
+// PO as higher-level API helps to create reusable codes
+
+const { test, expect } = require('@playwright/test');
+const { PlaywrightDevPage } = require('./Pages/home');
+
+test('getting started should contain table of contents', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.getStarted();
+  await expect(playwrightDev.tocList).toHaveText([
+    `How to install Playwright`,
+    `What's Installed`,
+    `How to run the example test`,
+    `How to open the HTML test report`,
+    `Write tests using web first assertions, page fixtures and locators`,
+    `Run single test, multiple tests, headed mode`,
+    `Generate tests with Codegen`,
+    `See a trace of your tests`
+  ]);
+});
+
+test('should show Page Object Model article', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.pageObjectModel();
+  await expect(page.locator('article')).toContainText('Page Object Model is a common pattern');
+});
